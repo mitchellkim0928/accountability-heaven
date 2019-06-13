6af90f81b7379a1e96dbf587be95e2fcfb2fb101
Mitchell Kim <mitchellkim0928@gmail.com>
2019-06-13 13:05:48 -0700

    typo in postback

--- index.js
+++ index.js
@@ -135,9 +135,9 @@ function handlePostback(sender_psid, received_postback) {
   let payload = received_postback.payload;
   // set the response based on the postback payload
   if (payload === 'yes') {
-    response = {"test": "Thanks!"};
+    response = {"text": "Thanks!"};
   } else if (payload === 'no') {
-    response = {"test": "Oops, try sending another image."};
+    response = {"text": "Oops, try sending another image."};
   }
 
   callSendAPI(sender_psid, response);
